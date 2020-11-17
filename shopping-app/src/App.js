efaa4fa5cce91953995976ecdfa0f24bd5ebc43f
willjantscher <william.jantscher@gmail.com>
2020-11-17 11:51:00 -0700

    some list stuff

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -4,12 +4,15 @@
 // npm install express
 // npx create-react-app shopping-app
 
+// https://github.com/willjantscher/Docker_Shopping_app.git
 // to run: npm start in /src cd
 
 
 import './App.css';
 import React from 'react'
 import List from './list'
+import AddItem from './addItem'
+
 
 
 class App extends React.Component  {
@@ -17,12 +20,36 @@ class App extends React.Component  {
     super(props)
     this.state = {
       list: [{
-        item: 'banannas',
+        name: 'banannas',
         qty: 4,
         unit: 'bunch',
       }], //item, qty, unit
+      tempItem: [],
     }
   }
+
+
+handleAddToCart = (e) => {
+  console.log('adding to cart')
+  console.log(e.target)
+}
+
+handleRemoveItem = (e) => {
+  console.log('removing item')
+}
+
+handleInputChange = (e) => {
+  let item = [];
+  item = this.state.tempItem;
+  item[e.target.id] = e.target.value;
+  this.setState({tempItem: item})
+  //console.log(this.state.tempItem)
+}
+
+handleAddItem = (e) => {
+
+}
+
 
   render () {
     return (
@@ -31,6 +58,12 @@ class App extends React.Component  {
         <p>
           <List
             list={this.state.list}
+            onAddToCart={this.handleAddToCart}
+            onRemoveItem={this.handleRemoveItem}
+          />
+          <AddItem
+            onInputChange={this.handleInputChange}
+            onAddItem={this.handleAddItem}
           />
         </p>
         <a>
--- shopping-app/src/list.js
+++ shopping-app/src/list.js
@@ -4,19 +4,25 @@ const List = (props) => {
     let shoppingList = props.list.map((item) => {
         return (
                 <tr>
-                    <td>{item.item}</td>
+                    <td>{item.name}</td>
                     <td>{item.qty}</td>
                     <td>{item.unit}</td>
+                    <td><input onClick={props.onAddToCart}  type="button" value="Add to cart"/></td>
+                    <td><input onClick={props.onRemoveItem} type="button" value="Remove item"/></td>
                 </tr>
         )
     })
     return (
         <table>
+
             <thead>
+                
                 <tr>
                     <td>Item</td>
                     <td>Quantity</td>
                     <td>Unit</td>
+                    <td></td>
+                    <td></td>
                 </tr>
             </thead>
             <tbody>{shoppingList}</tbody>
